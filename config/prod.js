dabd7b4b567c423ca72193658087b8a7b229775b
TajaMaughan <42856210+TajaMaughan@users.noreply.github.com>
2019-06-06 16:59:25 -0600

    finish sendgrid setup. email sending. click tracking working. click rerouting working

--- config/prod.js
+++ config/prod.js
@@ -7,5 +7,6 @@ module.exports = {
     googleRedirectURI: 'https://fast-brushlands-42503.herokuapp.com/auth/google/callback',
     stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
     stripeSecretKey: process.env.STRIPE_SECRET_KEY,
-    sendGridKey: process.env.SEND_GRID_KEY
+    sendGridKey: process.env.SEND_GRID_KEY,
+    redirectDomain: process.env.REDIREC_DOMAIN
 }
