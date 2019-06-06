3e6ba0c47fff9c0a566b528503339c4f62834479
TajaMaughan <42856210+TajaMaughan@users.noreply.github.com>
2019-06-06 15:53:44 -0600

    finish sendgrid

--- services/Mailer.js
+++ services/Mailer.js
@@ -6,18 +6,49 @@ class Mailer extends helper.Mail {
   constructor({ subject, recipients }, content) {
     super();
 
+    this.sgApi = sendgrid(keys.sendGridKey); 
     this.from_email = new helperer.Email('no-reply@emaily.com');
     this.subject = subject;
     this.body = new helper.Content('text/html', content);
     this.recipients = this.formatAddresses(recipients);
 
     this.addContent(this.body);
+    this.addClickTracking();
+    this.addRecipients();
   }
 
   formatAddresses(recipients) {
     return recipients.map(({ email }) => {
       return new helper.Email(email);
     })
+  }
+
+  addClickTracking() {
+    const trackingSettings = new helper.TrackingSettings();
+    const clickTracking = new helper.ClickTracking(true, true);
+
+    trackingSettings.setClickTracking(clickTracking);
+    this.addTrackingSettings(trackingSettings);
+  }
+
+  addRecipients() {
+    const personalize = new helper.Personalization();
+
+    this.recipeints.forEach(recipient = > {
+      personalize.addTo(recipient);
+    })
+    this.addPersonalization(personalize);
+  }
+
+  async send() {
+    const request = this.agApi.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: this.toJSON()
+    })
+    
+    const response = this.sgApi.API(request);
+    return response;
   }
 }
 
