741d87d0342eb4c3622065561cfef5bbd0bddafb
TajaMaughan <42856210+TajaMaughan@users.noreply.github.com>
2019-06-11 17:09:52 -0600

    form sending email and redirecting after submit

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -33,7 +33,7 @@ class SurveyForm extends Component {
 function validate(values) {
   const errors = {};
 
-  errors.emails = validateEmails(values.emails || '');
+  errors.recipients = validateEmails(values.recipients || '');
 
   _.map(formFields, ({ name }) => {
     if (!values[name]) {
